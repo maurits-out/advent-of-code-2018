c11e7230233f79fbbfcbdd4dcf9cc7e12e8668c7
maurits <17969131+maurits-out@users.noreply.github.com>
2021-05-09 12:25:45 +0200

    Day 2 part 1.

--- day02/spec/day02/core_spec.clj
+++ day02/spec/day02/core_spec.clj
@@ -1,7 +1,39 @@
 (ns day02.core-spec
   (:require [speclj.core :refer :all]
-            [day02.core :refer :all]))
+            [day02.core :refer :all]
+            [clojure.java.io :as io]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(def input-file (io/resource "input.txt"))
+
+(describe "Day 1"
+
+  (context "Parsing"
+    (it "should be able to parse the input."
+      (should= ["abcdef", "bababc", "abbcde"] (parse-input "abcdef\nbababc\nabbcde\n"))))
+
+  (context "Counting letters"
+    (it "should be able to determine if an ID contains a specific count of any letter."
+      (should-not ((has-any-letter-count-fn 2) "abcdef"))
+      (should-not ((has-any-letter-count-fn 3) "abcdef"))
+      (should ((has-any-letter-count-fn 2) "bababc"))
+      (should ((has-any-letter-count-fn 3) "bababc"))))
+
+  (context "Counting IDs"
+    (it "should be able to count IDs that have exactly two of any letter."
+      (should= 4 (count-ids ["abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"] 2)))
+    (it "should be able to count IDs that have exacvtly three of any letter."
+      (should= 3 (count-ids ["abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"] 3))))
+
+  (context "Checksum"
+    (it "should be able to calculate the checksum given the counts."
+      (should= 12 (calculate-checksum 3 4))
+      (should= 56 (calculate-checksum 7 8))))
+
+  (context "Examples"
+    (it "should solve part 1."
+      (should= 12 (solve1 "abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab\n"))))
+
+  (context "Solutions"
+    (let [input (slurp input-file)]
+      (it "should solve part 1."
+        (should= 5456 (solve1 input))))))
--- day02/src/day02/core.clj
+++ day02/src/day02/core.clj
@@ -1,5 +1,19 @@
-(ns day02.core)
+(ns day02.core
+  (:require [clojure.string :as string]))
 
-(defn -main
-  [& args]
-  (println "Hello World"))
+(defn parse-input [input]
+  (string/split-lines input))
+
+(defn has-any-letter-count-fn [count]
+  (fn [id] (some #{count} (vals (frequencies id)))))
+
+(defn count-ids [ids letter-count]
+  (let [has-any-letter-count (has-any-letter-count-fn letter-count)]
+    (count (filter #(has-any-letter-count %) ids))))
+
+(defn calculate-checksum [x y]
+  (* x y))
+
+(defn solve1 [input]
+  (let [ids (parse-input input)]
+    (calculate-checksum (count-ids ids 2) (count-ids ids 3))))
