a994a117749ea8e8c8baa4502f2c4a3f172d9891
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2021-07-10 20:48:08 +0200

    Updated description.

--- day01/project.clj
+++ day01/project.clj
@@ -1,5 +1,5 @@
 (defproject day01 "0.1.0-SNAPSHOT"
-  :description "Day 1"
+  :description "Chronal Calibration"
   :main day01.core
   :dependencies [[org.clojure/clojure "1.10.3"]]
   :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
--- day02/project.clj
+++ day02/project.clj
@@ -1,5 +1,5 @@
 (defproject day02 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Inventory Management System"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
