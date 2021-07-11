57034c6d5cb14af9b18caaa9d60be7dab1784979
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2021-07-11 19:37:19 +0200

    Day 3 part 1.

--- day03/spec/day03/core_spec.clj
+++ day03/spec/day03/core_spec.clj
@@ -1,7 +1,50 @@
 (ns day03.core-spec
   (:require [speclj.core :refer :all]
-            [day03.core :refer :all]))
+            [day03.core :refer :all]
+            [clojure.java.io :as io]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(def input-file (io/resource "input.txt"))
+
+(describe "Day 2"
+
+  (context "Parsing"
+
+    (it "should be able to parse a claim."
+      (should== ["123" 3 2 5 4] (parse-claim "#123 @ 3,2: 5x4"))
+      (should== ["13" 288 876 18 16] (parse-claim "#13 @ 288,876: 18x16")))
+    (it "should be able to parse the input."
+      (should== [["123" 3 2 5 4] ["13" 288 876 18 16]]
+        (parse-input "#123 @ 3,2: 5x4\n#13 @ 288,876: 18x16\n"))))
+
+  (context "Converting to square inches"
+
+    (it "should be able to convert a claim to square inches."
+      (should== [[3 2] [3 3] [3 4] [3 5]
+                 [4 2] [4 3] [4 4] [4 5]
+                 [5 2] [5 3] [5 4] [5 5]
+                 [6 2] [6 3] [6 4] [6 5]
+                 [7 2] [7 3] [7 4] [7 5]]
+        (convert-claim-to-square-inches ["123" 3 2 5 4])))
+
+    (it "should be able to create a collection of all square inches of multiple claims."
+      (should== [] (convert-claims-to-square-inches []))
+      (should== [[1 7] [1 8] [2 7] [2 8]] (convert-claims-to-square-inches [["1" 1 7 2 2]]))
+      (should== [[1 7] [1 8] [2 7] [2 8] [3 5]] (convert-claims-to-square-inches [["1" 1 7 2 2] ["2" 3 5 1 1]]))))
+
+  (context "Counting"
+
+    (it "should be able to count the number of square inches of fabric within two or more claims."
+      (should= 0 (count-square-inches-within-at-least-2-claims [[1 7] [1 8] [2 7] [2 8] [3 5]]))
+      (should= 4 (count-square-inches-within-at-least-2-claims [[3 1] [4 1] [5 1] [6 1]
+                                                                [3 2] [4 2] [5 2] [6 2]
+                                                                [3 3] [4 3] [5 3] [6 3]
+                                                                [3 4] [4 4] [5 4] [6 4]
+                                                                [1 3] [2 3] [3 3] [5 3]
+                                                                [1 4] [2 4] [3 4] [5 4]
+                                                                [1 5] [2 5] [3 5] [5 5]
+                                                                [1 6] [2 6] [3 6] [5 6]]))))
+
+  (context = "Solutions"
+
+    (it "should solve part 1"
+      (should= 110383 (solve1 (slurp input-file))))))
--- day03/src/day03/core.clj
+++ day03/src/day03/core.clj
@@ -1,5 +1,28 @@
-(ns day03.core)
+(ns day03.core
+  (:require [clojure.string :as string]))
 
-(defn -main
-  [& args]
-  (println "Hello World"))
+(def claim-regex #"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)")
+
+(defn parse-claim [claim]
+  (let [[_ id left top width height] (re-matches claim-regex claim)]
+    [id (Integer/parseInt left) (Integer/parseInt top) (Integer/parseInt width) (Integer/parseInt height)]))
+
+(defn parse-input [input]
+  (mapv parse-claim (string/split-lines input)))
+
+(defn convert-claim-to-square-inches [[_ left top width height]]
+  (for [x (range left (+ left width)) y (range top (+ top height))] [x y]))
+
+(defn convert-claims-to-square-inches [claims]
+  (apply concat (map convert-claim-to-square-inches claims)))
+
+(defn count-square-inches-within-at-least-2-claims [square-inches]
+  (->>
+    (frequencies square-inches)
+    (filter (fn [[_ num-square-inches]] (> num-square-inches 1)))
+    (count)))
+
+(defn solve1 [input]
+  (let [claims (parse-input input)
+        square-inches (convert-claims-to-square-inches claims)]
+    (count-square-inches-within-at-least-2-claims square-inches)))
