8376a42bd0869bd0d5bcb5c8bcbc40c54b910d5f
maurits <17969131+maurits-out@users.noreply.github.com>
2021-07-19 15:06:11 +0200

    Day 3.

--- day03/spec/day03/core_spec.clj
+++ day03/spec/day03/core_spec.clj
@@ -10,10 +10,11 @@
   (context "Parsing"
 
     (it "should be able to parse a claim."
-      (should== ["123" 3 2 5 4] (parse-claim "#123 @ 3,2: 5x4"))
-      (should== ["13" 288 876 18 16] (parse-claim "#13 @ 288,876: 18x16")))
+      (should== [123 3 2 5 4] (parse-claim "#123 @ 3,2: 5x4"))
+      (should== [13 288 876 18 16] (parse-claim "#13 @ 288,876: 18x16")))
     (it "should be able to parse the input."
-      (should== [["123" 3 2 5 4] ["13" 288 876 18 16]]
+      (should== [{:id 123, :left 3, :top 2, :width 5, :height 4}
+                 {:id 13, :left 288, :top 876, :width 18, :height 16}]
         (parse-input "#123 @ 3,2: 5x4\n#13 @ 288,876: 18x16\n"))))
 
   (context "Converting to square inches"
@@ -24,12 +25,13 @@
                  [5 2] [5 3] [5 4] [5 5]
                  [6 2] [6 3] [6 4] [6 5]
                  [7 2] [7 3] [7 4] [7 5]]
-        (convert-claim-to-square-inches ["123" 3 2 5 4])))
+        (convert-claim-to-square-inches {:id 123, :left 3, :top 2, :width 5, :height 4})))
 
     (it "should be able to create a collection of all square inches of multiple claims."
       (should== [] (convert-claims-to-square-inches []))
-      (should== [[1 7] [1 8] [2 7] [2 8]] (convert-claims-to-square-inches [["1" 1 7 2 2]]))
-      (should== [[1 7] [1 8] [2 7] [2 8] [3 5]] (convert-claims-to-square-inches [["1" 1 7 2 2] ["2" 3 5 1 1]]))))
+      (should== [[1 7] [1 8] [2 7] [2 8]] (convert-claims-to-square-inches [{:id 1, :left 1, :top 7, :width 2, :height 2}]))
+      (should== [[1 7] [1 8] [2 7] [2 8] [3 5]] (convert-claims-to-square-inches [{:id 1, :left 1, :top 7, :width 2, :height 2}
+                                                                                  {:id 2, :left 3, :top 5, :width 1, :height 1}]))))
 
   (context "Counting"
 
@@ -44,7 +46,21 @@
                                                                 [1 5] [2 5] [3 5] [5 5]
                                                                 [1 6] [2 6] [3 6] [5 6]]))))
 
-  (context = "Solutions"
+  (context "Overlapping"
+
+    (it "should be able to tell if two claims overlap with each other."
+      (should-not (overlapping? {:id 1, :left 1, :top 3, :width 4, :height 4} {:id 3, :left 5, :top 5, :width 2, :height 2}))
+      (should-not (overlapping? {:id 2, :left 3, :top 1, :width 4, :height 4} {:id 3, :left 5, :top 5, :width 2, :height 2}))
+      (should (overlapping? {:id 1, :left 1, :top 3, :width 4, :height 4} {:id 2, :left 3, :top 1, :width 4, :height 4})))
+
+    (it "should find the claim that does not overlap with any other claim."
+      (should= [3] (ids-of-non-overlapping-claim [{:id 1, :left 1, :top 3, :width 4, :height 4} {:id 2, :left 3, :top 1, :width 4, :height 4} {:id 3, :left 5, :top 5, :width 2, :height 2}]))))
+
+  (context "Solutions"
 
     (it "should solve part 1"
-      (should= 110383 (solve1 (slurp input-file))))))
+      (should= 110383 (solve1 (slurp input-file))))
+
+    (it "should solve part 2"
+      (should= 129 (solve2 (slurp input-file))))))
+
--- day03/src/day03/core.clj
+++ day03/src/day03/core.clj
@@ -4,13 +4,18 @@
 (def claim-regex #"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)")
 
 (defn parse-claim [claim]
-  (let [[_ id left top width height] (re-matches claim-regex claim)]
-    [id (Integer/parseInt left) (Integer/parseInt top) (Integer/parseInt width) (Integer/parseInt height)]))
+  (->>
+    (re-matches claim-regex claim)
+    (rest)
+    (mapv #(Integer/parseInt %))))
+
+(defn claim-values->map [[id left top width height]]
+  {:id id :left, left :top, top :width width, :height height})
 
 (defn parse-input [input]
-  (mapv parse-claim (string/split-lines input)))
+  (mapv (comp claim-values->map parse-claim) (string/split-lines input)))
 
-(defn convert-claim-to-square-inches [[_ left top width height]]
+(defn convert-claim-to-square-inches [{:keys [left top width height]}]
   (for [x (range left (+ left width)) y (range top (+ top height))] [x y]))
 
 (defn convert-claims-to-square-inches [claims]
@@ -19,10 +24,28 @@
 (defn count-square-inches-within-at-least-2-claims [square-inches]
   (->>
     (frequencies square-inches)
-    (filter (fn [[_ num-square-inches]] (> num-square-inches 1)))
+    (filter (fn [[_ count]] (> count 1)))
     (count)))
 
 (defn solve1 [input]
   (let [claims (parse-input input)
         square-inches (convert-claims-to-square-inches claims)]
     (count-square-inches-within-at-least-2-claims square-inches)))
+
+(defn overlapping? [{left1 :left top1 :top width1 :width height1 :height}
+                    {left2 :left top2 :top width2 :width height2 :height}]
+  (and
+    (< left1 (+ left2 width2))
+    (> (+ left1 width1) left2)
+    (< top1 (+ top2 height2))
+    (> (+ top1 height1) top2)))
+
+(defn ids-of-non-overlapping-claim [claims]
+  (for [claim claims
+        :when (not-any? #(and (not= claim %) (overlapping? claim %))
+                claims)]
+    (:id claim)))
+
+(defn solve2 [input]
+  (let [claims (parse-input input)]
+    (first (ids-of-non-overlapping-claim claims))))
