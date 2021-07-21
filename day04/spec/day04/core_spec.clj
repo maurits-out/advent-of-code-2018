a1d1de42ae0c72310c91432f95d25b629b7983ec
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2021-07-21 12:23:44 +0200

    Day 4 part 1.

--- day04/spec/day04/core_spec.clj
+++ day04/spec/day04/core_spec.clj
@@ -1,7 +1,68 @@
 (ns day04.core-spec
   (:require [speclj.core :refer :all]
-            [day04.core :refer :all]))
+            [day04.core :refer :all]
+            [clojure.java.io :as io]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(def input-file (io/resource "input.txt"))
+
+(describe "Day 4"
+
+  (context "Parsing input"
+
+    (it "should be able to parse a 'falls asleep' record."
+      (should== {:day "1518-11-02", :hour "00", :minutes "40", :type :fall-asleep}
+        (parse-record "[1518-11-02 00:40] falls asleep")))
+
+    (it "should be able to parse a 'wakes up' record."
+      (should== {:day "1518-11-02", :hour "00", :minutes "50", :type :wakes-up}
+        (parse-record "[1518-11-02 00:50] wakes up")))
+
+    (it "should be able to parse a 'begins shift' record."
+      (should== {:day "1518-11-03", :hour "00", :minutes "05", :type :begins-shift, :guard "10"}
+        (parse-record "[1518-11-03 00:05] Guard #10 begins shift")))
+
+    (it "should be able to parse multiple records"
+      (let [input "[1518-11-02 00:40] falls asleep\n[1518-11-02 00:50] wakes up\n[1518-11-03 00:05] Guard #10 begins shift\n"]
+        (should= [{:day "1518-11-02", :hour "00", :minutes "40", :type :fall-asleep}
+                  {:day "1518-11-02", :hour "00", :minutes "50", :type :wakes-up}
+                  {:day "1518-11-03", :hour "00", :minutes "05", :type :begins-shift, :guard "10"}]
+          (parse-records input))))
+
+    (it "should sort records by date"
+      (let [input "[1518-11-03 00:05] Guard #10 begins shift\n[1518-11-02 00:50] wakes up\n[1518-11-02 00:40] falls asleep\n"]
+        (should= [{:day "1518-11-02", :hour "00", :minutes "40", :type :fall-asleep}
+                  {:day "1518-11-02", :hour "00", :minutes "50", :type :wakes-up}
+                  {:day "1518-11-03", :hour "00", :minutes "05", :type :begins-shift, :guard "10"}]
+          (parse-records input)))))
+
+  (context "Replay records"
+
+    (it "should keep track which guard begins a shift."
+      (should= {:guard "10", :minutes-asleep-by-guard {}}
+        (replay {:minutes-asleep-by-guard {}} {:day "1518-11-03", :hour "00", :minutes "05", :type :begins-shift, :guard "10"}))
+      (should= {:guard "99"}
+        (replay {} {:day "1518-11-01", :hour "23", :minutes "58", :type :begins-shift, :guard "99"})))
+
+    (it "should keep track when a guard falls asleep."
+      (should= {:guard "10", :sleep-start "40"}
+        (replay {:guard "10"} {:day "1518-11-02", :hour "00", :minutes "40", :type :fall-asleep})))
+
+    (it "should record the minutes when a guard wakes up."
+      (should= {:guard "10", :minutes-asleep-by-guard {"10" [5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24]}}
+        (replay {:guard "10", :sleep-start "05", :minutes-asleep-by-guard {}}
+          {:day "1518-11-01", :hour "00", :minutes "25", :type :wakes-up}))
+      (should= {:guard "10", :minutes-asleep-by-guard {"10" [1 2 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24]}}
+        (replay {:guard "10", :sleep-start "05", :minutes-asleep-by-guard {"10" [1 2]}}
+          {:day "1518-11-01", :hour "00", :minutes "25", :type :wakes-up}))))
+
+  (context "Finding guard"
+
+    (it "should find the guard that has the most minutes asleep."
+      (should= "10" (find-guard-with-most-minutes-asleep {"5" [1 2], "10" [5 6 7]})))
+    (it "should find the minute that a guard spends asleep the most"
+      (should= 3 (find-minute-most-asleep [1 2 3 3 4 5 6 6 3]))))
+
+  (context "Solutions"
+
+    (it "should solve part 1"
+      (should= 67558 (solve1 (slurp input-file))))))
--- day04/src/day04/core.clj
+++ day04/src/day04/core.clj
@@ -1,5 +1,45 @@
-(ns day04.core)
+(ns day04.core
+  (:require [clojure.string :as string]))
 
-(defn -main
-  [& args]
-  (println "Hello World"))
+(def regex-falls-asleep #"\[(.{10}) (\d{2}):(\d{2})\] falls asleep")
+(def regex-falls-wakes-up #"\[(.{10}) (\d{2}):(\d{2})\] wakes up")
+(def regex-falls-begins-shift #"\[(.{10}) (\d{2}):(\d{2})\] Guard #(\d+) begins shift")
+
+(defn parse-record [line]
+  (if-let [[_ day hour minutes] (re-matches regex-falls-asleep line)]
+    {:day day, :hour hour, :minutes minutes, :type :fall-asleep}
+    (if-let [[_ day hour minutes] (re-matches regex-falls-wakes-up line)]
+      {:day day, :hour hour, :minutes minutes, :type :wakes-up}
+      (let [[_ day hour minutes guard] (re-matches regex-falls-begins-shift line)]
+        {:day day, :hour hour, :minutes minutes, :type :begins-shift, :guard guard}))))
+
+(defn parse-records [input]
+  (let [lines (string/split-lines input)]
+    (sort-by (juxt :day :hour :minutes) (mapv parse-record lines))))
+
+(defmulti replay (fn [_ record] (:type record)))
+(defmethod replay :begins-shift [state {:keys [guard]}]
+  (assoc state :guard guard))
+(defmethod replay :fall-asleep [state {:keys [minutes]}]
+  (assoc state :sleep-start minutes))
+(defmethod replay :wakes-up [{:keys [guard sleep-start minutes-asleep-by-guard]} {:keys [minutes]}]
+  (let [range (range (Integer/parseInt sleep-start) (Integer/parseInt minutes))
+        minutes-asleep (get minutes-asleep-by-guard guard [])]
+    {:guard                   guard,
+     :minutes-asleep-by-guard (assoc minutes-asleep-by-guard guard (concat minutes-asleep range))}))
+
+(defn replay-records [records]
+  (reduce replay {:minutes-asleep-by-guard {}} records))
+
+(defn find-guard-with-most-minutes-asleep [asleep]
+  (key (apply max-key (comp count val) asleep)))
+
+(defn find-minute-most-asleep [minutes]
+  (key (apply max-key val (frequencies minutes))))
+
+(defn solve1 [input]
+  (let [records (parse-records input)
+        minutes-asleep-by-guard (:minutes-asleep-by-guard (replay-records records))
+        guard (find-guard-with-most-minutes-asleep minutes-asleep-by-guard)
+        minute (find-minute-most-asleep (get minutes-asleep-by-guard guard))]
+    (* (Integer/parseInt guard) minute)))
