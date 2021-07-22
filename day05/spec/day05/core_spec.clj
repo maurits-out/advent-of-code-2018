3054deb0b45d690b65ef53b9edae7c885c228bd6
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2021-07-22 10:21:31 +0200

    Day 5.

---
+++ day05/spec/day05/core_spec.clj
@@ -0,0 +1,7 @@
+(ns day05.core-spec
+  (:require [speclj.core :refer :all]
+            [day05.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ day05/src/day05/core.clj
@@ -0,0 +1,5 @@
+(ns day05.core)
+
+(defn -main
+  [& args]
+  (println "Hello World"))
