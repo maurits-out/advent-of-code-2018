e5fb9576187aa9ca2d8ecd4eb61b26e4a1b577bb
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2021-07-26 10:47:38 +0200

    Day 6

--- day06/src/day06/core.clj
+++ day06/src/day06/core.clj
@@ -21,9 +21,9 @@
       nil)))
 
 (defn partial-view-coordinates [coordinates]
-  (let [x (mapv #(first %) coordinates)
-        y (mapv #(second %) coordinates)]
-    [[(dec (apply min x)) (dec (apply min y))] [(inc (apply max x)) (inc (apply max y))]]))
+  (let [x-values (mapv #(first %) coordinates)
+        y-values (mapv #(second %) coordinates)]
+    [[(dec (apply min x-values)) (dec (apply min y-values))] [(inc (apply max x-values)) (inc (apply max y-values))]]))
 
 (defn calculate-index-of-closest-coordinates-in-partial-view [coordinates [min-x min-y] [max-x max-y]]
   (for [x (range min-x (inc max-x))
@@ -54,23 +54,24 @@
   (let [[[min-x min-y] [max-x max-y]] (partial-view-coordinates coordinates)]
     [(quot (+ min-x max-x) 2) (quot (+ min-y max-y) 2)]))
 
-(defn generate-border [[x y] length]
-  (concat
-    (for [i (range 0 length)] [(+ x i) y])
-    (for [i (range 1 length)] [(dec (+ x length)) (+ y i)])
-    (for [i (range 0 (dec length))] [(+ x i) (dec (+ y length))])
-    (for [i (range 1 (dec length))] [x (+ y i)])))
+(defn generate-border [[center-x center-y] iteration]
+  (let [x (- center-x iteration)
+        y (- center-y iteration)
+        length (+ (* iteration 2) 1)]
+    (concat
+      (for [dx (range 0 length)] [(+ x dx) y])
+      (for [dy (range 1 length)] [(+ x (dec length)) (+ y dy)])
+      (for [dx (range 0 (dec length))] [(+ x dx) (+ y (dec length))])
+      (for [dy (range 1 (dec length))] [x (+ y dy)]))))
 
 (defn calculate-total-distance [coordinates location]
   (apply + (map #(manhattan-distance location %) coordinates)))
 
 (defn part2 [input total-distance]
   (let [coordinates (parse-input input)
-        [center-x center-y] (center-coordinates-of-partial-view coordinates)]
+        center (center-coordinates-of-partial-view coordinates)]
     (loop [iteration 0 current-size 0]
-      (let [[start-x start-y] [(- center-x iteration) (- center-y iteration)]
-            length (+ (* iteration 2) 1)
-            locations (for [l (generate-border [start-x start-y] length)
+      (let [locations (for [l (generate-border center iteration)
                             :when (< (calculate-total-distance coordinates l) total-distance)]
                         l)]
         (if (empty? locations)
