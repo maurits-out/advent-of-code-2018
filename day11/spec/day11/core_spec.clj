9654b534fc024b3efd7ec56df905e2a3dbad1e32
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2021-09-28 21:09:17 +0200

    Day 11 part 2.

--- day11/spec/day11/core_spec.clj
+++ day11/spec/day11/core_spec.clj
@@ -4,15 +4,8 @@
 
 (describe "Day 11"
 
-          (it "should calculate the power level of a fuel cell"
-              (should= 4 (power-level 3 5 8))
-              (should= -5 (power-level 122 79 57))
-              (should= 0 (power-level 217 196 39))
-              (should= 4 (power-level 101 153 71)))
-
-          (it "should calculate the power of a square."
-              (should= 29 (square-power 33 45 18))
-              (should= 30 (square-power 21 61 42)))
-
           (it "should solve part 1."
-              (should= [33 54] (part1 5235))))
+              (should= "33,54" (part1)))
+
+          (it "should solve part 2."
+              (should= "232,289,8" (part2))))
--- day11/src/day11/core.clj
+++ day11/src/day11/core.clj
@@ -1,9 +1,10 @@
-(ns day11.core)
+(ns day11.core
+  (:require [clojure.string :as string]))
 
 (def grid-size 300)
-(def square-size 3)
+(def grid-serial-number 5235)
 
-(defn power-level [x y grid-serial-number]
+(defn power-level [x y]
   (let [rack-id (+ x 10)
         pl1 (* y rack-id)
         pl2 (+ pl1 grid-serial-number)
@@ -11,19 +12,46 @@
         pl4 (rem (quot pl3 100) 10)]
     (- pl4 5)))
 
-(defn square-power [left-x top-y grid-serial-number]
-  (apply + (for [x (range left-x (+ left-x square-size))
-                 y (range top-y (+ top-y square-size))]
-             (power-level x y grid-serial-number))))
+(defn partial-sum [partial-sums x y]
+  (+ (power-level x y)
+     (partial-sums [(dec x) y])
+     (partial-sums [x (dec y)])
+     (- (partial-sums [(dec x) (dec y)]))))
 
-(defn square-coordinates []
-  (let [r (range (- grid-size (dec square-size)))]
+(defn generate-partial-sums-row [partial-sums y]
+  (reduce
+    #(assoc %1 [%2 y] (partial-sum %1 %2 y))
+    (assoc partial-sums [0 y] 0)
+    (range 1 (inc grid-size))))
+
+(defn generate-partial-sums []
+  (reduce
+    #(generate-partial-sums-row %1 %2)
+    (into (hash-map) (for [x (range 0 (inc grid-size))] [[x 0] 0]))
+    (range 1 (inc grid-size))))
+
+(defn square-power-level [[left-x top-y] size partial-sums]
+  (let [x (+ left-x (dec size))
+        y (+ top-y (dec size))]
+    (+ (partial-sums [x y])
+       (- (partial-sums [(- x size) y]))
+       (- (partial-sums [x (- y size)]))
+       (partial-sums [(- x size) (- y size)]))))
+
+(defn square-coordinates [size]
+  (let [r (range 1 (+ grid-size (- size) 2))]
     (for [x r, y r] [x y])))
 
-(defn square-powers [grid-serial-number]
-  (into (hash-map)
-        (for [[x y] (square-coordinates)]
-          [[x y] (square-power x y grid-serial-number)])))
+(defn square-with-largest-power-level [size partial-sums]
+  (apply max-key #(square-power-level % size partial-sums) (square-coordinates size)))
 
-(defn part1 [grid-serial-number]
-  (key (apply max-key val (square-powers grid-serial-number))))
+(defn part1 []
+  (let [partial-sums (generate-partial-sums)]
+    (string/join "," (square-with-largest-power-level 3 partial-sums))))
+
+(defn part2 []
+  (let [partial-sums (generate-partial-sums)
+        [[x y] size] (->> (range 1 (inc grid-size))
+                          (map (fn [size] [(square-with-largest-power-level size partial-sums) size]))
+                          (apply max-key (fn [[c size]] (square-power-level c size partial-sums))))]
+    (string/join "," [x y size])))
