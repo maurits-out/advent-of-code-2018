a29aba14fa05a6c57694adace9678db73a6a490c
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2021-10-12 21:19:22 +0200

    Day 14.

---
+++ day14/project.clj
@@ -0,0 +1,7 @@
+(defproject day14 "0.1.0-SNAPSHOT"
+  :description "Chocolate Charts"
+  :main day14.core
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
+  :plugins [[speclj "3.3.2"]]
+  :test-paths ["spec"])
---
+++ day14/spec/day14/core_spec.clj
@@ -0,0 +1,7 @@
+(ns day14.core-spec
+  (:require [speclj.core :refer :all]
+            [day14.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
