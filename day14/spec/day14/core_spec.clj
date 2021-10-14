d7942aae1e961d4c753c5689c901cbc30a1ad3c4
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2021-10-14 21:35:01 +0200

    Day 14.

--- day14/spec/day14/core_spec.clj
+++ day14/spec/day14/core_spec.clj
@@ -2,6 +2,19 @@
   (:require [speclj.core :refer :all]
             [day14.core :refer :all]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(describe "Chocolate Charts"
+          (context "Part 1"
+                   (it "should find the scores of ten recipes immediately after the number of recipes in the input"
+                       (should= "5158916779" (count-recipes-scores-after 9))
+                       (should= "0124515891" (count-recipes-scores-after 5))
+                       (should= "9251071085" (count-recipes-scores-after 18))
+                       (should= "5941429882" (count-recipes-scores-after 2018))
+                       (should= "5371393113" (count-recipes-scores-after 513401))))
+
+          (context "Part 2"
+                   (it "should count the recipes to the left of the score sequence"
+                       (should= 9 (count-recipes-to-the-left [5 1 5 8 9]))
+                       (should= 5 (count-recipes-to-the-left [0 1 2 4 5]))
+                       (should= 18 (count-recipes-to-the-left [9 2 5 1 0]))
+                       (should= 2018 (count-recipes-to-the-left [5 9 4 1 4]))
+                       (should= 20286858 (count-recipes-to-the-left [5 1 3 4 0 1])))))
--- day14/src/day14/core.clj
+++ day14/src/day14/core.clj
@@ -1,5 +1,43 @@
-(ns day14.core)
+(ns day14.core
+  (:require [clojure.string :as string]))
 
-(defn -main
-  [& args]
-  (println "Hello World"))
+(defn initial-state []
+  {:score-board [3 7] :pos-elf-1 0 :pos-elf-2 1})
+
+(defn update-elf-position [recipe current-position score-board-size]
+  (rem (+ current-position recipe 1) score-board-size))
+
+(defn next-recipe [{:keys [score-board pos-elf-1 pos-elf-2]}]
+  (let [recipe-elf-1 (nth score-board pos-elf-1)
+        recipe-elf-2 (nth score-board pos-elf-2)
+        recipe-sum (+ recipe-elf-1 recipe-elf-2)
+        digits-of-sum (if (< recipe-sum 10) [recipe-sum] [1 (rem recipe-sum 10)])
+        updated-score-board (apply conj score-board digits-of-sum)
+        next-pos-elf-1 (update-elf-position recipe-elf-1 pos-elf-1 (count updated-score-board))
+        next-pos-elf-2 (update-elf-position recipe-elf-2 pos-elf-2 (count updated-score-board))]
+    {:score-board updated-score-board :pos-elf-1 next-pos-elf-1 :pos-elf-2 next-pos-elf-2}))
+
+(defn generate-recipes-while [predicate]
+  (loop [[current & states] (iterate next-recipe (initial-state))]
+    (if (predicate (:score-board current))
+      (recur states)
+      (:score-board current))))
+
+(defn count-recipes-scores-after [input]
+  (->> (generate-recipes-while #(< (count %) (+ input 10)))
+       (drop input)
+       (take 10)
+       (string/join)))
+
+(defn is-match [input score-board start]
+  (let [end (+ start (count input))]
+    (if (and (>= start 0) (= (subvec score-board start end) input))
+      start)))
+
+(defn index-of-match [input score-board]
+  (let [start (- (count score-board) (count input))]
+    (or (is-match input score-board start) (is-match input score-board (dec start)))))
+
+(defn count-recipes-to-the-left [input]
+  (->> (generate-recipes-while #(not (index-of-match input %)))
+       (index-of-match input)))
