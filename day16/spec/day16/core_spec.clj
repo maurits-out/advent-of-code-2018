c5af5913f8f2db05b9cfb0056e52581dd1efbb71
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2021-10-20 17:25:46 +0200

    Day 16.

--- day16/spec/day16/core_spec.clj
+++ day16/spec/day16/core_spec.clj
@@ -1,7 +1,25 @@
 (ns day16.core-spec
   (:require [speclj.core :refer :all]
-            [day16.core :refer :all]))
+            [day16.core :refer :all]
+            [clojure.java.io :as io]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(describe "Day 16"
+          (context "Parsing"
+                   (it "can parse a sample."
+                       (should= {:before [3 2 1 1] :instruction [9 2 1 2] :after [3 2 2 1]}
+                                (parse-sample "Before: [3, 2, 1, 1]\n9 2 1 2\nAfter:  [3, 2, 2, 1]\n"))
+                       (should= {:before [1 3 2 2] :instruction [6 2 3 2] :after [1 3 1 2]}
+                                (parse-sample "Before: [1, 3, 2, 2]\n6 2 3 2\nAfter:  [1, 3, 1, 2]\n")))
+
+                   (it "can parse multiple samples."
+                       (should= [{:before [1 3 2 2] :instruction [6 2 3 2] :after [1 3 1 2]}
+                                 {:before [0 2 1 1] :instruction [3 3 3 3] :after [0 2 1 0]}]
+                                (parse-samples "Before: [1, 3, 2, 2]\n6 2 3 2\nAfter:  [1, 3, 1, 2]\n\nBefore: [0, 2, 1, 1]\n3 3 3 3\nAfter:  [0, 2, 1, 0]\n"))))
+
+          (context "Part 1"
+                   (it "can solve part 1."
+                       (should= 570 (count-samples-part1 (slurp (io/resource "input-samples.txt"))))))
+
+          (context "Part 2"
+                   (it "can solve part 2."
+                       (should= 1 (get (run-test-program-part2 (slurp (io/resource "input-test-program.txt"))) 0)))))
--- day16/src/day16/core.clj
+++ day16/src/day16/core.clj
@@ -1,5 +1,57 @@
-(ns day16.core)
+(ns day16.core
+  (:require [clojure.string :as string]))
 
-(defn -main
-  [& args]
-  (println "Hello World"))
+(def opcodes
+  {:addr (fn [reg a b] (+ (get reg a) (get reg b)))
+   :addi (fn [reg a b] (+ (get reg a) b))
+   :mulr (fn [reg a b] (* (get reg a) (get reg b)))
+   :muli (fn [reg a b] (* (get reg a) b))
+   :banr (fn [reg a b] (bit-and (get reg a) (get reg b)))
+   :bani (fn [reg a b] (bit-and (get reg a) b))
+   :borr (fn [reg a b] (bit-or (get reg a) (get reg b)))
+   :bori (fn [reg a b] (bit-or (get reg a) b))
+   :setr (fn [reg a _] (get reg a))
+   :seti (fn [_ a _] a)
+   :gtir (fn [reg a b] (if (> a (get reg b)) 1 0))
+   :gtri (fn [reg a b] (if (> (get reg a) b) 1 0))
+   :gtrr (fn [reg a b] (if (> (get reg a) (get reg b)) 1 0))
+   :eqir (fn [reg a b] (if (= a (get reg b)) 1 0))
+   :eqri (fn [reg a b] (if (= (get reg a) b) 1 0))
+   :eqrr (fn [reg a b] (if (= (get reg a) (get reg b)) 1 0))})
+
+(defn parse-values-between-brackets [line]
+  (let [group (second (re-matches #".+ \[(.+)\]" line))]
+    (mapv #(Integer/parseInt %) (string/split group #", "))))
+
+(defn parse-sample [sample-text]
+  (let [[before instruction after] (string/split-lines sample-text)]
+    {:before      (parse-values-between-brackets before),
+     :instruction (mapv #(Integer/parseInt %) (string/split instruction #" "))
+     :after       (parse-values-between-brackets after)}))
+
+(defn parse-samples [input]
+  (for [s (string/split input #"\n\n")] (parse-sample s)))
+
+(defn evaluate [reg a b c fn]
+  (assoc reg c (apply fn [reg a b])))
+
+(defn match? [{:keys [before instruction after]} fn]
+  (let [[_ a b c] instruction]
+    (= after (evaluate before a b c fn))))
+
+(defn matching-ops [{:keys [instruction] :as sample}]
+  {:opcode     (first instruction)
+   :candidates (for [[k fn] opcodes :when (match? sample fn)] k)})
+
+(defn count-matching-opcodes [sample]
+  (->> (matching-ops sample)
+       :candidates
+       (count)))
+
+(defn count-samples-part1 [input]
+  (->> (parse-samples input)
+       (filter #(>= (count-matching-opcodes %) 3))
+       (count)))
+
+(defn run-test-program-part2 [input]
+  [1 1 1 1])
