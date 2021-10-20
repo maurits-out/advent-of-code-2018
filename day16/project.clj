863cb884c4f4b5a1bb536633fb1cffb8d553aa65
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2021-10-20 12:27:29 +0200

    Day 16.

---
+++ day16/project.clj
@@ -0,0 +1,7 @@
+(defproject day16 "0.1.0-SNAPSHOT"
+  :description "Chronal Classification"
+  :main day16.core
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
+  :plugins [[speclj "3.3.2"]]
+  :test-paths ["spec"])
---
+++ day16/spec/day16/core_spec.clj
@@ -0,0 +1,7 @@
+(ns day16.core-spec
+  (:require [speclj.core :refer :all]
+            [day16.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
