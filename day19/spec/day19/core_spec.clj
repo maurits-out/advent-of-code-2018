1fe7815ec3e04940220a04842571765e62874cba
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2021-10-30 21:52:02 +0200

    Day 19.

--- day19/spec/day19/core_spec.clj
+++ day19/spec/day19/core_spec.clj
@@ -5,9 +5,22 @@
 (def example "#ip 0\nseti 5 0 1\nseti 6 0 2\naddi 0 1 0\naddr 1 2 3\nsetr 1 0 0\nseti 8 0 4\nseti 9 0 5\n")
 
 (describe "Day 19"
+
           (context "Parsing"
                    (it "should parse the input."
                        (should= {:ip-reg 0,
-                                 :program ["seti 5 0 1" "seti 6 0 2" "addi 0 1 0"
-                                           "addr 1 2 3" "setr 1 0 0" "seti 8 0 4"
-                                           "seti 9 0 5"]} (parse-input example)))))
+                                 :program [["seti" 5 0 1] ["seti" 6 0 2] ["addi" 0 1 0]
+                                           ["addr" 1 2 3] ["setr" 1 0 0] ["seti" 8 0 4]
+                                           ["seti" 9 0 5]]} (parse-input example))))
+
+          (context "State"
+                   (it "should create the initial state."
+                       (should= {:ip 0, :ip-reg 3, :reg [0 0 0 0 0 0]} (initial-state 3))))
+
+          (context "Evaluate"
+                   (it "should evaluate an instruction."
+                       (should= [0 5 0 0 0 0] (evaluate [0 0 0 0 0 0] ["seti" 5 0 1])))
+                   (it "can write the ip to the registers."
+                       (should= [0 0 0 2 0 0] (write-ip-to-reg 2 3 [0 0 0 0 0 0])))
+                   (it "can extract the ip from the registers."
+                       (should= 2 (get-ip-from-reg 3 [0 0 0 2 0 0])))))
--- day19/src/day19/core.clj
+++ day19/src/day19/core.clj
@@ -1,9 +1,41 @@
 (ns day19.core
   (:require [clojure.string :as string]))
 
+(def op-fn
+  {"addr" (fn [reg a b] (+ (get reg a) (get reg b)))
+   "addi" (fn [reg a b] (+ (get reg a) b))
+   "mulr" (fn [reg a b] (* (get reg a) (get reg b)))
+   "muli" (fn [reg a b] (* (get reg a) b))
+   "banr" (fn [reg a b] (bit-and (get reg a) (get reg b)))
+   "bani" (fn [reg a b] (bit-and (get reg a) b))
+   "borr" (fn [reg a b] (bit-or (get reg a) (get reg b)))
+   "bori" (fn [reg a b] (bit-or (get reg a) b))
+   "setr" (fn [reg a _] (get reg a))
+   "seti" (fn [_ a _] a)
+   "gtir" (fn [reg a b] (if (> a (get reg b)) 1 0))
+   "gtri" (fn [reg a b] (if (> (get reg a) b) 1 0))
+   "gtrr" (fn [reg a b] (if (> (get reg a) (get reg b)) 1 0))
+   "eqir" (fn [reg a b] (if (= a (get reg b)) 1 0))
+   "eqri" (fn [reg a b] (if (= (get reg a) b) 1 0))
+   "eqrr" (fn [reg a b] (if (= (get reg a) (get reg b)) 1 0))})
+
+(defn parse-instruction [instruction]
+  (let [[op & args] (string/split instruction #" ")]
+    (into [op] (mapv #(Integer/parseInt %) args))))
+
 (defn parse-input [input]
-  (let [lines (string/split-lines input)
-        ip-reg (Character/digit ^char (last (first lines)) 10)
-        program (vec (next lines))]
-    {:ip-reg ip-reg,
-     :program program}))
+  (let [lines (string/split-lines input)]
+    {:ip-reg  (Character/digit ^char (last (first lines)) 10),
+     :program (mapv parse-instruction (next lines))}))
+
+(defn initial-state [ip-reg]
+  {:ip 0, :ip-reg ip-reg, :reg [0 0 0 0 0 0]})
+
+(defn evaluate [reg [op a b c]]
+  (assoc reg c (apply (op-fn op) [reg a b])))
+
+(defn write-ip-to-reg [ip ip-reg reg]
+  (assoc reg ip-reg ip))
+
+(defn get-ip-from-reg [ip-reg reg]
+  (get reg ip-reg))
