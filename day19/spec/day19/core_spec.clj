e34e3666ecb18adf1bc157903231170df2d8ce18
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2021-10-31 10:18:55 +0100

    Day 19.

--- day19/spec/day19/core_spec.clj
+++ day19/spec/day19/core_spec.clj
@@ -1,8 +1,10 @@
 (ns day19.core-spec
   (:require [speclj.core :refer :all]
-            [day19.core :refer :all]))
+            [day19.core :refer :all]
+            [clojure.java.io :as io]))
 
 (def example "#ip 0\nseti 5 0 1\nseti 6 0 2\naddi 0 1 0\naddr 1 2 3\nsetr 1 0 0\nseti 8 0 4\nseti 9 0 5\n")
+(def input-file (io/resource "input.txt"))
 
 (describe "Day 19"
 
@@ -13,14 +15,14 @@
                                            ["addr" 1 2 3] ["setr" 1 0 0] ["seti" 8 0 4]
                                            ["seti" 9 0 5]]} (parse-input example))))
 
-          (context "State"
-                   (it "should create the initial state."
-                       (should= {:ip 0, :ip-reg 3, :reg [0 0 0 0 0 0]} (initial-state 3))))
-
           (context "Evaluate"
                    (it "should evaluate an instruction."
-                       (should= [0 5 0 0 0 0] (evaluate [0 0 0 0 0 0] ["seti" 5 0 1])))
-                   (it "can write the ip to the registers."
-                       (should= [0 0 0 2 0 0] (write-ip-to-reg 2 3 [0 0 0 0 0 0])))
-                   (it "can extract the ip from the registers."
-                       (should= 2 (get-ip-from-reg 3 [0 0 0 2 0 0])))))
+                       (should= [0 5 0 0 0 0] (evaluate [0 0 0 0 0 0] ["seti" 5 0 1]))))
+
+          (context "Example"
+                   (it "should execute the example."
+                       (should= 6 (execute (parse-input example)))))
+
+          (context "Part 1"
+                   (it "should execute the puzzle input."
+                       (should= 2106 (execute (parse-input (slurp input-file)))))))
--- day19/src/day19/core.clj
+++ day19/src/day19/core.clj
@@ -28,14 +28,20 @@
     {:ip-reg  (Character/digit ^char (last (first lines)) 10),
      :program (mapv parse-instruction (next lines))}))
 
-(defn initial-state [ip-reg]
-  {:ip 0, :ip-reg ip-reg, :reg [0 0 0 0 0 0]})
-
 (defn evaluate [reg [op a b c]]
   (assoc reg c (apply (op-fn op) [reg a b])))
 
-(defn write-ip-to-reg [ip ip-reg reg]
-  (assoc reg ip-reg ip))
+(defn apply-instruction [{:keys [ip reg]} ip-reg instruction]
+  (let [updated-reg (evaluate (assoc reg ip-reg ip) instruction)
+        updated-ip (inc (get updated-reg ip-reg))]
+    {:ip updated-ip :ip-reg ip-reg :reg updated-reg}))
 
-(defn get-ip-from-reg [ip-reg reg]
-  (get reg ip-reg))
+(defn ip-out-of-range? [ip program]
+  (or (< ip 0) (>= ip (count program))))
+
+(defn execute [{:keys [ip-reg program]}]
+  (loop [state {:ip 0, :reg [0 0 0 0 0 0]}]
+    (if (ip-out-of-range? (:ip state) program)
+      (get (:reg state) 0)
+      (let [new-state (apply-instruction state ip-reg (get program (:ip state)))]
+        (recur new-state)))))
