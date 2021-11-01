571766fd8d8985ed79e50cfdbdee58909008f629
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2021-11-01 20:14:50 +0100

    Day 20

---
+++ day20/project.clj
@@ -0,0 +1,7 @@
+(defproject day20 "0.1.0-SNAPSHOT"
+  :description "A Regular Map"
+  :main day20.core
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
+  :plugins [[speclj "3.3.2"]]
+  :test-paths ["spec"])
---
+++ day20/src/day20/core.clj
@@ -0,0 +1,5 @@
+(ns day20.core)
+
+(defn -main
+  [& args]
+  (println "Hello World"))
