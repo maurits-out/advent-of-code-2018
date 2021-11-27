484b45407b14fe8a6f2bb3e1b2eff2c327e265e8
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2021-11-27 11:31:12 +0100

    Day 22.

--- day22/src/day22/core.clj
+++ day22/src/day22/core.clj
@@ -1,27 +1,29 @@
 (ns day22.core)
 
-(defn coordinates [tx ty]
+(defn coordinates [[tx ty]]
   (for [x (range 0 (inc tx))
         y (range 0 (inc ty))]
     [x y]))
 
-(defn erosion-levels [[tx ty] depth]
-  (reduce
-    (fn [m [x y]] (assoc m [x y] (let [g (cond
-                                           (or (= 0 x y) (and (= x tx) (= y ty))) 0
-                                           (= 0 x) (* 48271 y)
-                                           (= 0 y) (* 16807 x)
-                                           :else (* (m [(dec x) y]) (m [x (dec y)])))]
-                                   (rem (+ g depth) 20183))))
-    {}
-    (coordinates tx ty)))
+(defn erosion-level-fn [[tx ty] depth]
+  (let [erosion-level (fn [mem-erosion-level [x y]]
+                        (let [erosion-level (fn [c] (mem-erosion-level mem-erosion-level c))
+                              g (cond
+                                  (or (= 0 x y) (and (= x tx) (= y ty))) 0
+                                  (= 0 x) (* 48271 y)
+                                  (= 0 y) (* 16807 x)
+                                  :else (* (erosion-level [(dec x) y]) (erosion-level [x (dec y)])))]
+                          (rem (+ g depth) 20183)))
+        mem-erosion-level (memoize erosion-level)]
+    (partial mem-erosion-level mem-erosion-level)))
 
-(defn total-risk-level [target depth]
-  (->>
-    (erosion-levels target depth)
-    vals
-    (map #(rem % 3))
-    (apply +)))
+(defn part1 [target depth]
+  (let [erosion-level (erosion-level-fn target depth)]
+    (->>
+      (coordinates target)
+      (map erosion-level)
+      (map #(rem % 3))
+      (apply +))))
 
 (defn -main []
-  (println "Part 1:" (total-risk-level [12 757] 3198)))
+  (println "Part 1:" (part1 [12 757] 3198)))
