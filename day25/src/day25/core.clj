7a33dd54963b0c08747d949c2d19e1a167958ac8
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2023-12-31 15:10:36 +0100

    Simplified implementation.

--- day25/src/day25/core.clj
+++ day25/src/day25/core.clj
@@ -1,47 +1,32 @@
 (ns day25.core
   (:require [clojure.string :as string]))
 
-(def max-distance 3)
-
-(defn to-int [str]
-  (Integer/parseInt str))
-
-(defn split-line [line]
-  (string/split line #","))
-
 (defn parse-line [line]
-  (mapv to-int (split-line line)))
+  (map #(Integer/parseInt %) (string/split line #",")))
 
 (defn parse-input [input]
   (let [lines (string/split-lines input)]
-    (set (mapv parse-line lines))))
-
-(defn sum [numbers]
-  (reduce + numbers))
+    (set (map parse-line lines))))
 
 (defn manhattan-distance [point1 point2]
-  (sum (map #(abs (- %1 %2)) point1 point2)))
+  (reduce + (map #(abs (- %1 %2)) point1 point2)))
 
 (defn is-within-distance? [point1 point2]
-  (<= (manhattan-distance point1 point2) max-distance))
+  (<= (manhattan-distance point1 point2) 3))
 
 (defn find-neighbors [remaining point]
-  (filter #(is-within-distance? point %) remaining))
+  (filter (partial is-within-distance? point) remaining))
 
 (defn dfs [remaining point]
   (let [updated-remaining (disj remaining point)
         neighbors (find-neighbors updated-remaining point)]
     (reduce dfs updated-remaining neighbors)))
 
-(defn count-constellations [fixed-points]
+(defn solve [input]
   (loop [acc 0
-         remaining fixed-points]
+         remaining (parse-input input)]
     (if (empty? remaining)
       acc
       (let [point (first remaining)
             updated-remaining (dfs remaining point)]
         (recur (inc acc) updated-remaining)))))
-
-(defn solve [input]
-  (let [fixed-points (parse-input input)]
-    (count-constellations fixed-points)))
